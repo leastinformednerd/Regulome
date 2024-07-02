4b5191bd6667a8e1e47af9a43a4dc3a6527dc1e6
leastinformednerd <automagicwyvern@gmail.com>
2024-07-02 20:26:06 +1200

    elf tidying

--- lib/elf/src/lib.rs
+++ lib/elf/src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 
-use elf::{ElfBytes, endian::AnyEndian, parse::ParseError};
+use elf::{ElfBytes, endian::AnyEndian, parse::ParseError, CommonElfData};
 
 use x86_64::structures::paging::{
     RecursivePageTable
@@ -13,31 +13,43 @@ use mem::FrameAllocator;
 pub enum LoadLocation {
     Any,
     Exactly(VirtAddr),
-    LessThan(VirtAddr),
-    GreaterThan(VirtAddr)
+    LessThan(VirtAddr), // Probably going to be used mostly for sticking userspace programs in
+                        // lower half
+    GreaterThan(VirtAddr) // Probably going to be used mostly for sticking kernel in higher half
 }
 
 pub enum ElfLoadError {
     ElfHeaderParseError(ParseError),
-    WrongInstructionSet
+    IncorrectType,
+    WrongInstructionSet,
     CommonDataNotFound,
+    MissingSymTab
 
     // A panic handler is not guaranteed to exist (and be pretty) so I'm going to leave it up to 
     // the caller to deal with this
     NotImplemented
 }
 
+impl From<ParseError> for ElfLoadError {
+    fn from(value: ParseError) -> Self {
+        Self::ElfHeaderParseError(value)
+    }
+}
 
-fn load_shared_library<Allocator: FrameAllocator>(data: &[u8], allocator: &Allocator, page_table: &mut RecursivePageTable, load_location: LoadLocation) -> Result<x86_64::VirtAddr, ElfLoadError> {
+// Medium priority. Would be good, but like I can just statically link everything to begin with,
+// and any PIE when compiled for Regulome can just be PIC instead
+fn load_shared_library<Allocator: FrameAllocator>(data: &[u8], allocator: &Allocator, page_table: &mut RecursivePageTable, load_location: LoadLocation, elf_bytes: ElfBytes) -> Result<x86_64::VirtAddr, ElfLoadError> {
     return Err(NotImplemented)
 }
 
-fn load_executable<Allocator: FrameAllocator>(data: &[u8], allocator: &Allocator, page_table: &mut RecursivePageTable, load_location: LoadLocation) -> Result<x86_64::VirtAddr, ElfLoadError> {
+// Low priority, I think I get by with just PIC for a bit
+fn load_executable<Allocator: FrameAllocator>(data: &[u8], allocator: &Allocator, page_table: &mut RecursivePageTable, load_location: LoadLocation, elf_bytes: ElfBytes) -> Result<x86_64::VirtAddr, ElfLoadError> {
     return Err(NotImplemented)
 }
 
-fn load_relocatable<Allocator: FrameAllocator>(data: &[u8], allocator: &Allocator, page_table: &mut RecursivePageTable, load_location: LoadLocation) -> Result<x86_64::VirtAddr, ElfLoadError> {
-    return Err(NotImplemented)
+// High priroity, the kernel is one of these
+fn load_relocatable<Allocator: FrameAllocator>(data: &[u8], allocator: &Allocator, page_table: &mut RecursivePageTable, load_location: LoadLocation, elf_bytes: ElfBytes) -> Result<x86_64::VirtAddr, ElfLoadError> {
+    
 }
 
 
@@ -49,7 +61,9 @@ fn load_relocatable<Allocator: FrameAllocator>(data: &[u8], allocator: &Allocato
 ///
 /// * `page_table` - the page table for the data to be mapped into (must be recursive)
 ///
-/// * `load_location` - a hint to the location in virtual memory 
+/// * `load_location` - a hint to the location in virtual memory
+///
+/// #
 pub fn load<Allocator: FrameAllocator>(data: &[u8], allocator: &Allocator, page_table: &mut RecursivePageTable, load_location: LoadLocation) -> Result<x86_64::VirtAddr, ElfLoadError> {
     let elf_bytes = match ElfBytes::<AnyEndian>::minimal_parse(data) {
         Ok(elf_bytes) => elf_bytes,
@@ -66,9 +80,12 @@ pub fn load<Allocator: FrameAllocator>(data: &[u8], allocator: &Allocator, page_
         ET_REL
     };
 
+    
+
     return match elf_bytes.ehdr.e_type {
           ET_DYN => load_shared_library(data, allocator, page_table, load_location, elf_bytes),
           ET_EXEC => load_executable(data, allocator, page_table, load_location, elf_bytes),
-          ET_REL => load_relocatable(data, allocator, page_table, load_location, elf_bytes)
+          ET_REL => load_relocatable(data, allocator, page_table, load_location, elf_bytes),
+          _ => Err(IncorrectType)
     }
 }
